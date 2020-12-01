5534504b84064893b7aa2c551c1f3c4d57a7cb8f
DESKTOP-DRAMV4V\Ryo <r.sksk818@gmail.com>
2020-12-01 14:11:25 +0900

    コメント機能非同期処理実装、各viewレイアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,11 @@ class User < ApplicationRecord
   has_one_attached :image
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name, presence:true
 
   def already_favorited?(post)  #いいねをしたか判定をする
-    self.favorites.exists?(post_id: post.id) #self=current.userに結びついているいいねの中でこのmenu_idが存在しているか
+    self.favorites.exists?(post_id: post.id) #self=current.userに結びついているいいねの中でこのpost_idが存在しているか
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts do
     resource :favorites, only: [:create, :destroy]  #いいね機能のルート
+    resources :comments, only: [:create, :destroy]  #コメント機能のルート
   end
-  resources :users, only: [:show, :edit, :create, :update]
+  resources :users, only: [:show, :edit, :update]
 
   root 'mystery#index'
 end
