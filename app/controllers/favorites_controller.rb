9f3cea895a97b2cd9ceb2099e183aed5b6bfb5a3
DESKTOP-DRAMV4V\Ryo <r.sksk818@gmail.com>
2020-12-05 13:20:37 +0900

    いいねした投稿一覧機能を作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,17 @@
 class FavoritesController < ApplicationController
+  before_action :find_post
+
   def create
-    @post = Post.find(params[:post_id])
     @favorite = current_user.favorites.create(post_id: params[:post_id]) #current.userに結びついたいいねを作成
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     @favorite = current_user.favorites.find_by(post_id: @post.id) #favoriteを見つける
     @favorite.destroy
   end
+
+  private
+   def find_post
+     @post = Post.find(params[:post_id])
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :posts do
     resource :favorites, only: [:create, :destroy]  #いいね機能のルート
     resources :comments, only: [:create, :destroy]  #コメント機能のルート
   end
-  resources :users, only: [:show, :edit, :update]
+
+  resources :users, only: [:show, :edit, :update, :destroy] do
+    get 'favorites' => 'users#favorites'
+  end
 
   root 'mystery#index'
 end
